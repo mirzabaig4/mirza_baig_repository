ce2c0e0b39bc8c02e6a2e2d1d9da95e25ae7a8f4
Mirza Kashan Baig <baig_kashan@yahoo.com>
2021-10-11 20:53:21 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mirza Kashan Baig on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
